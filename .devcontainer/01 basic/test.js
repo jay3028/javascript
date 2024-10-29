0bc6bcce667f34aa71fcf92cb703e0aed14d365f
jay3028 <jaykumar0634@gmail.com>
2024-10-29 23:57:27 +0530

    ARRAY PART 2

--- .devcontainer/01 basic/test.js
+++ .devcontainer/01 basic/test.js
@@ -238,30 +238,30 @@
 // console.log(myn2);
 
 // --------------------- array part 2 ------------
-const marvel_heros = ["thor", "Ironman", "spiderman"];
-const dc_heros = ["superman", "flash", "batman"];
+// const marvel_heros = ["thor", "Ironman", "spiderman"];
+// const dc_heros = ["superman", "flash", "batman"];
 
-marvel_heros.push(dc_heros);
+// marvel_heros.push(dc_heros);
 
-console.log(marvel_heros);
+// console.log(marvel_heros);
 
-// const allHeros = marvel_heros.concat(dc_heros)
-// console.log(allHeros);
+// // const allHeros = marvel_heros.concat(dc_heros)
+// // console.log(allHeros);
 
-const all_new_heros = [...marvel_heros, ...dc_heros];
+// const all_new_heros = [...marvel_heros, ...dc_heros];
 
-console.log(all_new_heros);
+// console.log(all_new_heros);
 
-const another_array = [1, 2, 3, [4, 5, 6], 7, [8], [9, [4, 5]]];
+// const another_array = [1, 2, 3, [4, 5, 6], 7, [8], [9, [4, 5]]];
 
-const real_another_array = another_array.flat(Infinity);
+// const real_another_array = another_array.flat(Infinity);
 
-console.log(real_another_array);
-console.log(Array.isArray("Hitesh"));
-console.log(Array.from("Hitesh"));
-console.log(Array.from({ name: "hitesh" })); // interesting
+// console.log(real_another_array);
+// console.log(Array.isArray("Hitesh"));
+// console.log(Array.from("Hitesh"));
+// console.log(Array.from({ name: "hitesh" })); // interesting
 
-let score1 = 100;
-let score2 = 200;
-let score3 = 300;
-console.log(Array.of(score1, score2, score3));
+// let score1 = 100;
+// let score2 = 200;
+// let score3 = 300;
+// console.log(Array.of(score1, score2, score3));
